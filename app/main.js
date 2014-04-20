c330e145f4486064727ee1a77c0d79f8cd11378e
ovidiucs <ovidiu.ctin@gmail.com>
2014-04-20 20:50:04 +0300

    commit

--- app/main.js
+++ app/main.js
@@ -1,7 +1,24 @@
 //javascript-test.js
 
-var unique = require('uniq');
+//var unique = require('uniq');
 
-var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+//var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
 
-console.log(unique(data));
+//console.log(unique(data));
+
+
+var forms = require('forms'), 
+    fields = forms.fields,
+    validators = forms.validators;
+
+var reg_form = forms.create({
+        username: fields.string({ required: true}),
+        password: fields. password({required: true}),
+        confirm: fields.password({
+            required: true,
+            validators: [validators.matchField('password')]
+        }),
+        email: fields.email()
+});
+
+reg_form.toHTML();
