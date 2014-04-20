a3081dba5d0bbf8719dad0537d0c7fc2bc5f3835
ovidiucs <ovidiu.ctin@gmail.com>
2014-04-20 21:32:29 +0300

    browserify test

--- app/main.js
+++ app/main.js
@@ -13,7 +13,7 @@ var forms = require('forms'),
 
 var reg_form = forms.create({
         username: fields.string({ required: true}),
-        password: fields. password({required: true}),
+        password: fields.password({required: true}),
         confirm: fields.password({
             required: true,
             validators: [validators.matchField('password')]
