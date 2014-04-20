90b20f33285b34f797773d98a98a09169901ac2e
ovidiucs <ovidiu.ctin@gmail.com>
2014-04-20 21:59:14 +0300

    Test browserify

--- app/bundle.js
+++ app/bundle.js
@@ -21,6 +21,7 @@ var reg_form = forms.create({
     email: fields.email()
 });
 
+reg_form.toHTML();
 
 },{"forms":2}],2:[function(require,module,exports){
 // This file is just added for convenience so this repository can be
